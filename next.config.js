dd4d60bf2a5ec52cf695d8828c8c7a49c290208e
yilongchuan <1358395292@qq.com>
2025-03-28 23:33:07 +0800

    fix: configure webpack alias for @ imports

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
