456c25099a5e6bf13ad9c6d4237b6235afa0e85f
yilongchuan <1358395292@qq.com>
2025-03-28 23:35:52 +0800

    111

--- next.config.js
+++ next.config.js
@@ -8,13 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'),
-    };
-    return config;
-  },
 }
 
 module.exports = nextConfig 
